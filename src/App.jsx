70dd5a5c669d59a03f82f0bcc72492b4e4022e0c
Sagnik143 <sagnikbgemhs@gmail.com>
2026-03-19 21:45:15 +0530

    home page finalise

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import Header from './components/Header'
 import Footer from './components/Footer'
-import { Home } from 'lucide-react'
+import Home from "./pages/Home";
+import { Home as HomeIcon } from "lucide-react";
 
 const App = () => {
   return (
