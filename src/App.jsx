dd93fd73ddd907ab15a5576892923b0adb925a76
hridaydattavown-cyber <hridaydattvown@gmail.com>
2026-03-20 21:49:00 +0530

    features done

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import Home from "./pages/Home";
 import { Home as HomeIcon } from "lucide-react";
+import Features from './pages/Features';
 
 const App = () => {
   return (
     <div>
-    <Home/>
+    <Features />
     </div>
   )
 }
