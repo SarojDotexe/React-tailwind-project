6aba347ee0a604d1731529b784af9eadc2331dac
hridaydattavown-cyber <hridaydattvown@gmail.com>
2026-03-21 00:29:30 +0530

    hriday

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,18 @@
 import React from 'react'
 import Header from './components/Header'
 import Footer from './components/Footer'
-<<<<<<< HEAD
 import Home from "./pages/Home";
 
 import { Home as HomeIcon } from "lucide-react";
-<<<<<<< HEAD
-import Features from './pages/Features';
-=======
 import About from './About';
 import Contact from './pages/Contact';
->>>>>>> d7a61ba0d5af3bd762384266978900a6d0976cd7
-=======
-import Home from './pages/Home'
-import About from './About'
-import Services from './Services'
-import Gallery from './Gallery'
->>>>>>> 555a516445def36904137d844dd58c0676baefce
 
 const App = () => {
   return (
     <div>
-<<<<<<< HEAD
-<<<<<<< HEAD
-    <Features />
-=======
    <Header />
       <Contact />
       <Footer />
->>>>>>> d7a61ba0d5af3bd762384266978900a6d0976cd7
-=======
-      <Header />
-      <Home />
-      <Footer />
->>>>>>> 555a516445def36904137d844dd58c0676baefce
     </div>
   )
 }
